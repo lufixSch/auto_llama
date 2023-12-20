fd908e3215e4a2f7aa9462e64892d90d12d1a163
Lukas Schüttler <lukas.schuettler@ise-technology.com>
2023-12-20 16:11:10 +0100

    add date to chat history

--- pkg/auto_llama/_chat.py
+++ pkg/auto_llama/_chat.py
@@ -1,4 +1,5 @@
-from typing import Literal, TypeAlias
+from typing import Literal, TypeAlias, Callable
+from datetime import datetime
 
 ChatRoles: TypeAlias = Literal["system", "user", "assistant"]
 
@@ -6,9 +7,10 @@ ChatRoles: TypeAlias = Literal["system", "user", "assistant"]
 class ChatMessage:
     """Chat message"""
 
-    def __init__(self, role: ChatRoles, message: str):
+    def __init__(self, role: ChatRoles, message: str, date: datetime = None):
         self.role = role
         self.message = message
+        self.date = date or datetime.now()
 
 
 class Chat:
@@ -40,20 +42,30 @@ class Chat:
 
         return self._names[role]
 
-    def append(self, role: ChatRoles, message: str) -> "Chat":
+    def append(self, role: ChatRoles, message: str, date: datetime = None) -> "Chat":
         """Add message to the chat history"""
 
-        self._history.append(ChatMessage(role, message))
+        self._history.append(ChatMessage(role, message, date))
 
     def filter(
-        self, include_roles: list[ChatRoles] = ["user", "assistant", "system"], exclude_roles: list[ChatRoles] = []
+        self,
+        include_roles: list[ChatRoles] = ["user", "assistant", "system"],
+        exclude_roles: list[ChatRoles] = [],
+        filter_cb: Callable[[ChatMessage], bool] = lambda _: True,
     ) -> list[ChatMessage]:
-        """Filter chat history based on roles"""
+        """Filter chat history based on roles
+
+        Arguments:
+            include_roles (list[ChatRoles]): List of roles to include
+            exclude_roles (list[ChatRoles]): List of roles not to include
+            filter_cb (Callable[[ChatMessage], bool]): Custom filter function. Will be called for each message.
+            The message wil be included if the function returns `True`
+        """
 
         return [
             chat_msg
             for chat_msg in self.history
-            if chat_msg.role in include_roles and chat_msg.role not in exclude_roles
+            if (chat_msg.role in include_roles) and (chat_msg.role not in exclude_roles) and filter_cb(chat_msg)
         ]
 
     def last_from(self, role: ChatRoles) -> str:
